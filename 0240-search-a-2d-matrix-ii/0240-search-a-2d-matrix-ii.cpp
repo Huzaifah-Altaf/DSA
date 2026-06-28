3d38de8ca76b4e835130a9735a32d674afa11681
Huzaifah Altaf <huzaifahaltaf@gmail.com>
2026-06-28 23:15:22 +0530

    Time: 54 ms (54.05%), Space: 18.8 MB (38.06%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int i = 0, j = m - 1;
-        // int node = matrix[i][j];
+    
         while(i < n && j >= 0){
             int node = matrix[i][j];
             if(node == target) return 1;
