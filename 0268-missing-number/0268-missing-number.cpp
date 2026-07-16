0148de7d9d0a5e6fee7c2e3efc663b643415014f
Huzaifah Altaf <huzaifahaltaf@gmail.com>
2026-07-16 20:40:56 +0400

    Time: 0 ms (100%), Space: 21.6 MB (94.21%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int total_sum = (n*(n + 1))  / 2;
+        int array_sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            array_sum += nums[i];
+        }
+        return total_sum - array_sum;
+    }
+};
