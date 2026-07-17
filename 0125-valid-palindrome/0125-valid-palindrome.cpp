045ad71772c16e262617e5b5f1a320ed54dd6787
Huzaifah Altaf <huzaifahaltaf@gmail.com>
2026-07-18 00:27:29 +0400

    Time: 0 ms (100%), Space: 9.9 MB (54.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -7,27 +7,34 @@ public:
                 s[i] = tolower(s[i]);
                 i++;
             }
-            else if(s[i] <= '9' && s[i] >= '0'){
-                i++;
-            }
-            else if(s[i] <= 'z' && s[i] >= 'a'){
-                i++;
-            }
+            
             else{
-                s.erase(i, 1);
+                i++;
                 
             }
         }
         i = 0;
         int j = s.size() - 1;
         while(i <= j){
-            if(s[i] == s[j]){
+           
+            if(((s[i] <= '9' && s[i] >= '0') || (s[i] <= 'z' && s[i] >= 'a'))  && ((s[j] <= '9' && s[j] >= '0') || (s[j] <= 'z' && s[j] >= 'a'))){
+                 if(s[i] == s[j]){
                 i++;
                 j--;
             }
-            else{
+             else {
                 return false;
             }
+            }
+
+            else if (!(( s[i] <= '9' && s[i] >= '0') || (s[i] <= 'z' && s[i] >= 'a'))){
+                i++;
+            }
+            else{
+                j--;
+            }
+            
+           
         }
         return true;
     }
