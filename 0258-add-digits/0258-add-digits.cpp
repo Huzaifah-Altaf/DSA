68de5a9467fcc82f317f2ae1c6fb48b4a2171bd0
Huzaifah Altaf <huzaifahaltaf@gmail.com>
2026-07-19 14:16:54 +0400

    Time: 0 ms (100%), Space: 8.6 MB (80.52%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int digit = 0;
+        while(num / 10 != 0){
+            digit = 0;
+            while(num > 0){
+            digit += num % 10;
+            num = num / 10;
+        }
+        num = digit;
+        }
+        return num;
+    }
+};
